56ecd48ff81fda3cd18012a507f38baf5b6577d8
gabrieloliverlima <gabriel.oliver.001@hotmail.com>
2025-03-28 14:49:11 -0400

    Update changes

--- vms_terraform/datasource.tf
+++ vms_terraform/datasource.tf
@@ -1,3 +1,5 @@
 data "digitalocean_ssh_key" "ssh_key" {
   name = var.ssh_key_name
+  #public_key = file("~/.ssh/chave-ssh-terraform.pub")       #Irá criar a chave ssh dinamicamente. OBS.: Se realizar o Destroy do código, a chave também será destruída.
+                                                             #Não é indicado criar a chave dinamicamente quando irá usá-la em outros projetos
 }
